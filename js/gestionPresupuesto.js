ad28b04ba2a4b08394896f03664c146a33412487
Marta <martainformaticaaltozano@gmail.com>
2025-03-15 09:39:47 +0100

    crear, mostrar,actualizar
    las tres funciones antes de Objeto gasto
    Propiedades

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,41 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(valor) {
     // TODO
+    if(valor >= 0){
+        presupuesto = valor;
+        return presupuesto;
+    }else{
+        console.error("Error el valor no puede ser negativo")
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return `Tu presupuesto es de ${presupuesto}€`;
 }
 
-function CrearGasto() {
+function CrearGasto(valor) {
     // TODO
+    let gasto= {};
+    if(valor >= 0){
+        gasto.valor= valor;
+    }else{
+        gasto.valor= 0;
+    }
+    return gasto;
 }
+/*function CrearGasto(valor) {
+    if (valor >= 0) {
+        this.valor = valor;
+    } else {
+        this.valor = 0;
+    }
+}*/
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
