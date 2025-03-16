ead17352e53b650fb3fd7ce3c82e172bb8c6fa69
Marta <martainformaticaaltozano@gmail.com>
2025-03-16 09:41:27 +0100

    crear Gastos
    Crear los siguientes gastos (función crearGasto):
    ("Compra carne", 23.44, "2021-10-06", "casa", "comida")
    ("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida")
    ("Bonobús", 18.60, "2020-05-26", "transporte")
    ("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina")
    ("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros")
    ("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros")
    Añadir los gastos creados (función anyadirGasto)

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -4,3 +4,18 @@ import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js'
 gestionPresupuesto.actualizarPresupuesto(1500);
 
 gestionPresupuestoWeb.mostrarDatoEnId("presupuesto", gestionPresupuesto.mostrarPresupuesto());
+
+
+const gasto1 = new CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+const gasto2 = new CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+const gasto3 = new CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+const gasto4 = new CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+const gasto5 = new CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+const gasto6 = new CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+gestionPresupuesto.anyadirGasto(gasto1);
+gestionPresupuesto.anyadirGasto(gasto2);
+gestionPresupuesto.anyadirGasto(gasto3);
+gestionPresupuesto.anyadirGasto(gasto4);
+gestionPresupuesto.anyadirGasto(gasto5);
+gestionPresupuesto.anyadirGasto(gasto6);
