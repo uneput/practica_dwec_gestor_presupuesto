548c21d6c1818d606aedcc5d651845a0c3724b38
Marta <martainformaticaaltozano@gmail.com>
2025-03-16 09:31:00 +0100

    anyadir1500
    Actualizar el presupuesto a 1500€ (función actualizarPresupuesto)

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,2 +1,4 @@
 import * as gestionPresupuesto from './gestionPresupuesto.js';
 import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js'
+
+gestionPresupuesto.actualizarPresupuesto(1500);
