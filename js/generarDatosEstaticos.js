77c8c6e6bcfce62e72eb40a9258740a57451dc0a
Marta <martainformaticaaltozano@gmail.com>
2025-03-16 09:33:59 +0100

    presupuestom
    Mostrar el presupuesto en el div#presupuesto (funciones mostrarPresupuesto y mostrarDatoEnId)

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -2,3 +2,5 @@ import * as gestionPresupuesto from './gestionPresupuesto.js';
 import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js'
 
 gestionPresupuesto.actualizarPresupuesto(1500);
+
+gestionPresupuestoWeb.mostrarDatoEnId("presupuesto", gestionPresupuesto.mostrarPresupuesto());
