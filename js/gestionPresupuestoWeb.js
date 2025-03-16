1c887aa3c70868aa2a572333f85525a8f8ea9e59
Marta <martainformaticaaltozano@gmail.com>
2025-03-16 09:25:55 +0100

    funciones
    Función mostrarGastoWeb mostrarGastoAgrupadosWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -6,6 +6,44 @@ function mostrarDatoEnId(idElemento, valor){
     }
 }
 
+function mostrarGastoWeb(idElemento, gasto){
+    const contenedor = document.getElementById(idElemento);
+
+    const divGasto = document.createElement('div');
+    divGasto.classList.add('gasto');
+
+    divGasto.innerHTML = `
+        <div class="gasto-descripcion">${gasto.descripcion}</div>
+        <div class="gasto-fecha">${gasto.fecha}</div>
+        <div class="gasto-valor">${gasto.valor}</div>
+        <div class="gasto-etiquetas">
+            ${gasto.etiquetas.map(etiqueta => `<span class="gasto-etiquetas-etiqueta">${etiqueta}</span>`).join('')}
+        </div>
+    `;
+  
+    contenedor.appendChild(divGasto);
+}
+
+function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
+    //hace referencia a la practica 5
+    const contenedor = document.getElementById(idElemento);
+  
+    const divAgrupacion = document.createElement('div');
+    divAgrupacion.classList.add('agrupacion');
+  
+    divAgrupacion.innerHTML = `
+        <h1>Gastos agrupados por ${periodo}</h1>
+        ${Object.keys(agrup).map(key => `
+            <div class="agrupacion-dato">
+                <span class="agrupacion-dato-clave">${key}</span>
+                <span class="agrupacion-dato-valor">${agrup[key]}</span>
+            </div>
+        `).join('')}
+    `;
+  
+    contenedor.appendChild(divAgrupacion);
+}
+
 
 
 
